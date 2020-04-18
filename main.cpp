e4a661fbb5fdc8c292235b391d446815f7b494e3
P.wang <windowswangpei@hotmail.com>
2020-04-18 15:53:55 +0800

    draw first triangle

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "glm/glm.hpp"
@@ -13,6 +14,20 @@ void processInput(GLFWwindow* window)
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 }
+
+const char* vertexShaderSource = R"(#version 330 core
+	layout (location = 0) in vec3 aPos;
+	void main()
+	{
+	   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	})";
+
+const char* fragmentShaderSource = R"(#version 330 core
+			out vec4 FragColor;
+			void main()
+			{
+				FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+			})";
 
 int main()
 {
@@ -39,11 +54,95 @@ int main()
 		return -1;
 	}
 
+	const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+	const GLubyte* version = glGetString(GL_VERSION); // version as a string
+
+	const std::string title = std::string((char*)renderer) + ". OpenGL Version: " + std::string((char*)version);
+
+	glfwSetWindowTitle(window, title.c_str());
+
 	glViewport(0, 0, 800, 600);
 
-	glm::vec4 color_a{ 1.0f, 0.0f, 0.0f, 1.0f };
+	//  compile vertex shader and fregment shader
 
-	glm::vec4 color_b{ 0.0f, 1.0f, 0.0f, 1.0f };
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::Link_FAILED\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	float vertices[] = {
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex
+	// attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but
+	// this rarely happens. Modifying other VAOs requires a call to glBindVertexArray anyways so we
+	// generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+	// uncomment this call to draw in wireframe polygons.
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	glm::vec4 color_a{ 1.0f, 1.0f, 0.0f, 1.0f };
+
+	glm::vec4 color_b{ 0.0f, 1.0f, 1.0f, 1.0f };
 
 	float t = 1.0f;
 
@@ -69,15 +168,19 @@ int main()
 				}
 			}
 
-
 			glm::vec4 color_c = (1 - t) * color_a + t * color_b;
 
 			glClearColor(color_c.x, color_c.y, color_c.z, color_c.w);
 
 			glClear(GL_COLOR_BUFFER_BIT);
 
-		}
+			glUseProgram(shaderProgram);
 
+			glBindVertexArray(VAO);
+			//glDrawArrays(GL_TRIANGLES, 0, 3);
+
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		}
 
 		glfwPollEvents();
 
@@ -88,5 +191,3 @@ int main()
 
 	return 0;
 }
-
-
