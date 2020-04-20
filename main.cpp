78a78bfbcc7d161a0c0e6d7590af038c0241b591
P.wang <windowswangpei@hotmail.com>
2020-04-21 00:06:41 +0800

    test glm quaternion

--- main.cpp
+++ main.cpp
@@ -3,6 +3,38 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "glm/glm.hpp"
+//#debug begin
+#include "glm/gtc/quaternion.hpp"
+#include "glm/gtc/constants.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+//#debug end
+
+int main()
+{
+	const auto half_pi = glm::pi<float>() / 2.0f;
+	const auto fouth_pi = glm::pi<float>() / 4.0f;
+
+	const auto angle = glm::vec3{ 1.23f, 0.21f, half_pi };
+
+	const auto euler_z_matrix = glm::rotate(glm::mat4{ 1 }, angle.z, glm::vec3{0.f,0.f,1.0f});
+	const auto euler_y_matrix = glm::rotate(glm::mat4{ 1 }, angle.y, glm::vec3{0.f,1.f,0.0f});
+	const auto euler_x_matrix = glm::rotate(glm::mat4{ 1 }, angle.x, glm::vec3{1.f,0.f,0.0f});
+	const auto euler_matrix = euler_z_matrix*euler_y_matrix*euler_x_matrix;
+
+	const auto q = glm::quat{ angle };
+	const auto q_matrix = glm::mat4{ q };
+
+	for(int i = 0; i < 4; i++)
+		for(int j = 0; j < 4; j++)
+	{
+			assert(glm::epsilonEqual(euler_matrix[i][j], q_matrix[i][j], 0.001f));
+	}
+
+	return 0;
+
+}
+
+#if 0
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -27,6 +59,13 @@ const char* fragmentShaderSource = R"(#version 330 core
 			void main()
 			{
 				FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+			})";
+
+const char* fragmentShaderSource1 = R"(#version 330 core
+			out vec4 FragColor;
+			void main()
+			{
+				FragColor = vec4(0.1f, 0.3f, 0.8f, 1.0f);
 			})";
 
 int main()
@@ -98,33 +137,66 @@ int main()
 		std::cout << "ERROR::SHADER::VERTEX::Link_FAILED\n" << infoLog << std::endl;
 	}
 
+
+	unsigned int fragmentShader1;
+	fragmentShader1 = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader1, 1, &fragmentShaderSource1, NULL);
+	glCompileShader(fragmentShader1);
+	unsigned int shaderProgram1;
+	shaderProgram1 = glCreateProgram();
+
+	glAttachShader(shaderProgram1, vertexShader);
+	glAttachShader(shaderProgram1, fragmentShader1);
+	glLinkProgram(shaderProgram1);
+
+	glGetProgramiv(shaderProgram1, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram1, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::Link_FAILED\n" << infoLog << std::endl;
+	}
+
+
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+	glDeleteShader(fragmentShader1);
 
 	float vertices[] = {
-	 0.5f,  0.5f, 0.0f,  // top right
-	 0.5f, -0.5f, 0.0f,  // bottom right
-	-0.5f, -0.5f, 0.0f,  // bottom left
-	-0.5f,  0.5f, 0.0f   // top left 
+		0.0f, 0.0f, 0.0f,
+		0.0f, -1.0f, 0.f,
+		0.5f, 0.0f,  0.f,
+		0.0f, 1.0f,  0.0f,
+		-0.5f, 0.f,  0.0f
 	};
-	unsigned int indices[] = {  // note that we start from 0!
-		0, 1, 3,   // first triangle
-		1, 2, 3    // second triangle
+	unsigned int indices[] = {
+		1, 2, 0,
+		0, 2, 3,
+		3, 0, 4,
+		4, 0, 1,
 	};
 
-	unsigned int VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	unsigned int VBO[2], VAO[2], EBO[2];
+
+	glGenVertexArrays(2, VAO);
+	glGenBuffers(2, VBO);
+	glGenBuffers(2, EBO);
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-	glBindVertexArray(VAO);
+	glBindVertexArray(VAO[0]);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6*sizeof(float), indices, GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(VAO[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6*sizeof(float), indices + 6, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -176,10 +248,15 @@ int main()
 
 			glUseProgram(shaderProgram);
 
-			glBindVertexArray(VAO);
+			glBindVertexArray(VAO[0]);
 			//glDrawArrays(GL_TRIANGLES, 0, 3);
-
 			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
+			glUseProgram(shaderProgram1);
+			glBindVertexArray(VAO[1]);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 		}
 
 		glfwPollEvents();
@@ -191,3 +268,5 @@ int main()
 
 	return 0;
 }
+
+#endif
