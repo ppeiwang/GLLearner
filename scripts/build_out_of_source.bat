267666171ac9af942de1632505e0f58dc00c2b88
Pei <pei@gritworld.com>
2020-08-17 20:16:06 +0800

    update scripts

--- scripts/build_out_of_source.bat
+++ scripts/build_out_of_source.bat
@@ -2,6 +2,20 @@ cd ..
 if exist build == FALSE
 	md build
 
-cd build && cmake ..
+cd build && cmake -G"Visual Studio 16" ..
 
-if NOT ["%errorlevel%"]==["0"] pause
+if NOT ["%errorlevel%"]==["0"] GOTO HANG_ON
+
+@REM succ condition
+@REM check/open solution
+@IF EXIST .vs\openGL_show_case\v16\Browse.VC.opendb GOTO SUCC
+
+start openGL_show_case.sln
+
+@GOTO SUCC
+
+:HANG_ON
+@pause
+
+:SUCC
+@timeout /T 2
